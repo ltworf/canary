74a5a06e467cfece8d560f5ca48da0653d3e41e4
LtWorf <LtWorf@b902c34c-a72a-44ad-aa41-b0477b20fdf4>
2012-02-04 12:39:12 +0000

    error reporting facility
    git-svn-id: http://galileo.dmi.unict.it/svn/canary@8 b902c34c-a72a-44ad-aa41-b0477b20fdf4

---
+++ trunk/error.c
@@ -0,0 +1,35 @@
+/*
+Canary
+Copyright (C) 2012  Salvo "LtWorf" Tomaselli
+
+Canary is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
+*/
+
+#include "config.h"
+#include "error.h"
+
+#include <syslog.h>
+#include <unistd.h>
+
+/**
+ * Reports a fatal error and terminates
+ */
+void err_fatal(char* msg) {
+    openlog("canary monitor",LOG_PID | LOG_PERROR,LOG_AUTHPRIV);
+
+    syslog(LOG_CRIT,"%s",mgs);
+    exit(1);
+}
