67d0230541358e65f38fde77d77a500b76117ec5
LtWorf <LtWorf@b902c34c-a72a-44ad-aa41-b0477b20fdf4>
2012-02-09 16:01:03 +0000

    - creates the buffer type, for future use
    git-svn-id: http://galileo.dmi.unict.it/svn/canary@16 b902c34c-a72a-44ad-aa41-b0477b20fdf4

--- trunk/ca_alloc.c
+++ trunk/ca_alloc.c
@@ -2,7 +2,7 @@
 Canary
 Copyright (C) 2012  Salvo "LtWorf" Tomaselli
 
-Weborf is free software: you can redistribute it and/or modify
+Canary is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
@@ -23,16 +23,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdlib.h>
 
 #include "config.h"
+#include "types.h"
 
+//TODO remove this header
 #include <stdio.h>
+
 void* malloc(size_t size) {
     static void* (*real_malloc)(size_t) = NULL;
     if (!real_malloc)
         real_malloc = dlsym(RTLD_NEXT, "malloc");
 
-    void *p = real_malloc(size);
+    size_t bsize=size+(2*sizeof(canary_t));
+    void *p = real_malloc(bsize);
+    
+    buffer_t buf = {p,bsize};
+    
     fprintf(stderr, "malloc(%d) = %p\n", size, p);
     return p;
 }
 
+void free(void *ptr) {
+    static void* (*real_free)(void*) = NULL;
+    if (!real_free)
+        real_free = dlsym(RTLD_NEXT,"free");
+    
+    real_free(ptr);
+}
 // gcc -shared -ldl -fPIC jmalloc.c -o libjmalloc.so
