1f2afd6b8e2fee7a31a291cbed20e6ed59d849da
LtWorf <LtWorf@b902c34c-a72a-44ad-aa41-b0477b20fdf4>
2012-03-11 20:00:26 +0000

    - insert and visiting work
    git-svn-id: http://galileo.dmi.unict.it/svn/canary@36 b902c34c-a72a-44ad-aa41-b0477b20fdf4

--- trunk/ca_tree.c
+++ trunk/ca_tree.c
@@ -30,11 +30,61 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ca_alloc.h"
 
 static bool realloc_if_needed(queue_t*);
+static int t_get_leftmost(queue_t*,int);
+static int t_get_rightmost(queue_t*,int);
+static int t_get_left_parent(queue_t*,int);
+static int t_find(queue_t*,void*);
+
+static int t_find(queue_t*q,void*buf) {
+    int r=q->root;
+    
+    while (1) {
+        if (r==-1) {
+            return -1;
+        } else if (q->nodes[r].data < buf) {
+            r=q->nodes[r].right;
+        } else if (q->nodes[r].data > buf) { 
+            r=q->nodes[r].left;
+        } else {
+            return r;
+        }
+    }
+    
+}
+
+/**
+ * returns the index of the leftmost node from r
+ **/
+static int t_get_leftmost(queue_t* q ,int r) {
+    
+    while (1) {
+        if (q->nodes[r].left==-1)  {
+            return r;
+        } else {
+            r=q->nodes[r].left;
+        }
+    }
+}
+
+/**
+ * returns the index of the rightmost node from r
+ **/
+static int t_get_rightmost(queue_t* q ,int r) {
+    while (1) {
+        if (q->nodes[r].right==-1)  {
+            return r;
+        } else {
+            r=q->nodes[r].right;
+        }
+    }
+}
+
 static bool realloc_if_needed(queue_t* q) {
     //TODO make a realloc if needed
     if (q->space_left==0) return false;
     return true;
 }
+
 
 /**
  * Initializes the queue with "size" positions already.
@@ -56,11 +106,84 @@ bool q_init(queue_t* q,size_t size) {
 }
 
 /**
+ * removes the node at position index
+ * 
+ * WARNING: no checks on the size/existence are performed
+ **/
+static bool q_delete_index(queue_t*q,int index) {
+    
+    q_node_t deleted=q->nodes[index];
+    q->size--;
+    q->space_left++;
+    
+    if (q->size==0) { //Deleting last node
+        q->root=-1;
+        q->current=-1;
+        return true;
+    }
+    
+    //Move the last node in place of the deleted node, and update links
+    {
+        q->nodes[index]=q->nodes[q->size];
+        q_node_t *temp=&(q->nodes[q->nodes[index].parent]);
+    
+        if (temp->left==q->size) {
+            temp->left=index;
+        } else {
+            temp->right=index;
+        }
+    }
+    
+    //Deletion of the node
+    if (index==q->root) {
+        //TODO
+    }
+    
+    if (deleted.left==-1) {
+        if (deleted.right==-1) {
+            //deleting leaf
+            if (q->nodes[deleted.parent].left==index) {
+                q->nodes[deleted.parent].left=-1;
+            } else {
+                q->nodes[deleted.parent].right=-1;
+            }
+        } else {
+            //no left subtree, has right subtree
+            if (q->nodes[deleted.parent].left==index) {
+                q->nodes[deleted.parent].left=deleted.right;
+            } else {
+                q->nodes[deleted.parent].right=deleted.right;
+            }
+            
+            q->nodes[deleted.right].parent = deleted.parent;
+        }
+    } else {
+        if (deleted.right==-1) {
+            //only has left subtree
+            if (q->nodes[deleted.parent].left==index) {
+                q->nodes[deleted.parent].left=deleted.left;
+            } else {
+                q->nodes[deleted.parent].right=deleted.left;
+            }
+            q->nodes[deleted.left].parent = deleted.parent;
+            
+        } else {
+            //has both subtrees, must do a very ugly thing
+        }
+    }
+    
+}
+
+/**
  * removes an element from the tree
  * 
  * returns false if the removal has failed
  **/
-bool q_remove(queue_t*,void*) {
+bool q_remove(queue_t* q,void* b) {
+    int i=t_find(q,b);
+    if (i==-1) return false;
+    
+    q_delete_index(q,i);
         //TODO
 }
 
@@ -75,7 +198,7 @@ bool q_insert(queue_t* q, void* b) {
     q_node_t *node;
     node= &(q->nodes[q->size]);
     node->data=b;
-    node->left=
+    node->left=-1;
     node->right=-1;
     
     if (q->size>0) {
@@ -105,10 +228,9 @@ bool q_insert(queue_t* q, void* b) {
         
         node->parent=-1;
         
-        q->current=
+        q->current=0;
         q->root=0;
     }
-    
     
     q->size++;
     q->space_left--;
@@ -125,6 +247,25 @@ size_t q_get_size(queue_t* q) {
 }
 
 /**
+ * returns the id of the first parent located on
+ * the right of node i, or -1 if there is no such thing
+ **/
+static int t_get_right_parent(queue_t* q,int i) {
+    int p;
+    
+    while (1) {
+        p=q->nodes[i].parent;
+        if (p==-1) {
+            return -1;
+        } else if (q->nodes[p].left==i) {
+            return p;
+        } else {
+          i=p;
+        }
+    }
+}
+
+/**
  * Returns the "current" void*.
  * this function is used to iterate over
  * the data structure. The iteration
@@ -135,4 +276,24 @@ size_t q_get_size(queue_t* q) {
  * 
  **/
 void* q_get_current(queue_t* q) {
+    //printf("current:%d\n",q->current);
+    if (q->size==0) {
+        return NULL;
+    }
+    int current=q->current;
+    void* ret=q->nodes[current].data;
+    
+    if (q->nodes[current].right!=-1) {
+        q->current=t_get_leftmost(q,q->nodes[current].right);
+    } else {
+        q->current=t_get_right_parent(q,current);
+    }
+        
+    
+    //Resets the cycle when it ends
+    if (q->current==-1) {
+        q->current=t_get_leftmost(q,q->root);
+    }
+    
+    return ret;
 }
