fb9ffb585d0102fcecdeb4b746bb36d0345096d0
LtWorf <LtWorf@b902c34c-a72a-44ad-aa41-b0477b20fdf4>
2012-03-18 19:31:42 +0000

    Uses syn_queue to perform IPC
    git-svn-id: http://galileo.dmi.unict.it/svn/canary@59 b902c34c-a72a-44ad-aa41-b0477b20fdf4

--- trunk/ca_monitor.c
+++ trunk/ca_monitor.c
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ca_pageinfo.h"
 #include "ca_alloc.h"
 #include "error.h"
+#include "queue.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -36,16 +37,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <unistd.h>
 #include <time.h>
 
-static int in_monitor_pipe[2];
-static int out_monitor_pipe[2];
-
-#define PIPE_READ 0
-#define PIPE_WRITE 1
-
 void* monitor() __attribute__ ((noreturn));
 static inline void delay() ;
 
-static pthread_once_t thread_is_initialized = PTHREAD_ONCE_INIT;
+static syn_queue_t in_out_buffers;  //Buffers to be monitored
 
 /**
  * Inits the monitor thread, and the 
@@ -57,13 +52,15 @@ static void ca_init_thread() {
     
     if (initialized) return;
     
-    if (pthread_mutex_trylock(&cs_mutex)!=0) return;
-
-    if (
-        pipe2(in_monitor_pipe,O_CLOEXEC|O_NONBLOCK) + 
-        pipe2(out_monitor_pipe,O_CLOEXEC|O_NONBLOCK)
-        !=0
-    ) {
+    if (pthread_mutex_trylock(&cs_mutex)!=0) {
+        //another thread is initializing
+        //waiting for the other thread to finish and returning
+        //pthread_mutex_lock(&cs_mutex);
+        //pthread_mutex_unlock(&cs_mutex);
+        return;
+    }
+    
+    if (q_init(&in_out_buffers,MONITOR_PIPE_SIZE) !=0 ) {
         err_fatal("Unable to create IPC structures.");
     }
     
@@ -83,44 +80,18 @@ static void ca_init_thread() {
  * Adds a buffer to be monitored
  **/
 void ca_monitor_buffer(buffer_t buffer) {
-    //pthread_once(&thread_is_initialized, ca_init_thread); //Initializes the monitor at the first malloc
     ca_init_thread();
-    static pthread_mutex_t cs_mutex = PTHREAD_MUTEX_INITIALIZER;
-    int res=-1;
-    
     ca_init(buffer.ptr,buffer.size);
-
-    pthread_mutex_lock(&cs_mutex);
-    while (res<1) {
-        /*
-         * Since pipes are non blocking, cycles until the data is written.
-         * Hopefully it will work at the 1st attempt in most cases
-         */
-        void* zero=0;
-        
-        res=write(in_monitor_pipe[PIPE_WRITE],&(buffer.ptr),sizeof(void*));
-        res=write(in_monitor_pipe[PIPE_WRITE],&zero,sizeof(void*));
-        //printf("in %p\n",buffer.ptr);
-    }
-    pthread_mutex_unlock(&cs_mutex);
+    syn_buffer_t p={buffer.ptr,SYN_MONITOR_IN};
+    q_put(&in_out_buffers,p);
 }
 
 /**
  * Removes a buffer from being monitored
  **/
 void ca_unmonitor_ptr(void* ptr) {
-
-    static pthread_mutex_t cs_mutex = PTHREAD_MUTEX_INITIALIZER;
-    int res=-1;
-    pthread_mutex_lock(&cs_mutex);
-    while (res<1) {
-        /*
-         * Since pipes are non blocking, cycles until the data is written.
-         * Hopefully it will work at the 1st attempt in most cases
-         */
-        res=write(out_monitor_pipe[PIPE_WRITE],&(ptr),sizeof(void*));
-    }
-    pthread_mutex_unlock(&cs_mutex);
+    syn_buffer_t b={ptr,SYN_MONITOR_OUT};
+    q_put(&in_out_buffers,b);
 }
 
 /**
@@ -152,26 +123,32 @@ void* monitor() {
         err_fatal("Unable to allocate space for the list of buffers.");
     
     while (true) {
+        
         delay();
+        //printf("b");
         n++;
+        
         if (n % (1<<MONITOR_CHECK_PIPES)==0) {
-            void* new_buffer;
-            int r = read(in_monitor_pipe[PIPE_READ],&new_buffer,sizeof(void*));
             
-            if (r>=0) {
-                //printf("-> %p\n",new_buffer);
-                if (!q_insert(&hot,new_buffer)) {
-                    err_fatal("Unable to allocate space for the list of buffers.");
+            syn_buffer_t new_buffer=q_get(&in_out_buffers);
+            
+            if (new_buffer.ptr!=NULL) {
+                printf("get %p direction=%s\n",new_buffer.ptr, (new_buffer.direction==SYN_MONITOR_IN?"in":"out"));
+                if (new_buffer.direction==SYN_MONITOR_IN) {
+                    bool r=q_insert(&hot,new_buffer.ptr);
+                    printf("added %d\n",q_get_size(&hot));
+                    if (!r)
+                        err_fatal("Unable to allocate space for the list of buffers.");
+                        
+                } else {
+                    printf("---");
+                    __real_free(new_buffer.ptr);
+                    q_remove(&hot,new_buffer.ptr);
+                    q_remove(&cold,new_buffer.ptr);
                 }
             }
-            
-            int s = read(in_monitor_pipe[PIPE_READ],&new_buffer,sizeof(void*));
-            if (s>=0) {
-                __real_free(new_buffer);
-                q_remove(&hot,new_buffer);
-                q_remove(&cold,new_buffer);
-            }
         }
+        
         
         //Check a cold buffer and eventually moves it to hot
         if (n % (1<<MONITOR_CHECK_COLD)==0) {
@@ -185,7 +162,7 @@ void* monitor() {
         buffer = q_get_current(&hot);
         
         if (buffer==NULL) {
-            sleep(MONITOR_EMPTY_SLEEP);
+            //sleep(MONITOR_EMPTY_SLEEP);
             continue;
         } else if (!pgi_dirty(buffer)) {
                 q_insert(&cold,buffer);
